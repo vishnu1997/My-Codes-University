aba5b0ade5c41af0f1bce74779d3456e703e4193
Vishnu Vardhan Reddy <reddy.vishnu1997@gmail.com>
2017-02-08 07:06:15 +0530

    Threads

---
+++ Threads.java
@@ -0,0 +1,67 @@
+import java.io.*;
+
+
+
+class extendsThread extends Thread{
+  String name;
+  extendsThread(String name){
+    super(name);
+    this.name  = name;
+    System.out.println("Thread extended" + name);
+    start();
+  }
+  public void run(){
+    try{
+      for(int i=0;i<3;i++){
+        System.out.println("extended child thread: "+i+"   "+this);
+        sleep(500);
+      }
+    }catch(Exception e){
+      System.out.println("Exception caught in extended class");
+    }
+  }
+}
+
+class implementThread implements Runnable{
+  Thread r;
+  implementThread(){
+    System.out.println("implemnted thread created");
+    // new Thread(this,"implemented Thread").start(); this one can be used when no thread object r is declared inside the class
+    r = new Thread(this,"implemented Thread");
+    r.start();
+  }
+  public void run(){
+    try{
+      for(int i=0;i<3;i++){
+        System.out.println("implemented child thread: "+i+"  "+this);
+        Thread.sleep(1500);
+      }
+    }catch(Exception e){
+      System.out.println("Exception caught in implementThread class");
+    }
+  }
+
+}
+
+public class Threads{
+  public static void main(String argd[]){
+    Thread t = Thread.currentThread();
+    Thread c,d;
+    extendsThread a = new extendsThread("First");
+    extendsThread b = new extendsThread("Second");
+    implementThread z = new implementThread();
+    c = new extendsThread("third");
+    try{
+      System.out.println("Joining other threads in main");
+      a.join();
+      b.join();
+      z.r.join();
+    }catch(Exception e){
+      System.out.println("Error in main");
+    }
+    System.out.println("The name of the current thread is "+t);
+    t.setName("Maggi");
+    System.out.println("The new  name of the current thread is after changing"+t);
+  }
+
+}
