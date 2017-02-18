720bfcbe63ec22f809fbbe4a677cb54f7ac04117
vishnu vardhan reddy <reddy.vishnu1997@gmail.com>
2017-02-18 08:45:34 +0000

    exe5.java edited online with Bitbucket

--- exe5.java
+++ exe5.java
@@ -1,11 +1,15 @@
 import java.io.*;
 
+
+
+//-------------------------------------------queue
 class queue{
-	int arr[10],front,rear;
+	int arr[]=new int[10];
+	int front,rear;
 	boolean full,empty;
 		
 	queue(){
-		arr[]=0;
+		
 		rear = -1;
 		front = -1;
 		full = false;
@@ -15,39 +19,86 @@ class queue{
 	
 	synchronized void put(int i){
 		front++;
-		if(front>10){
-			wait();
+		if(front>10){	
+				try{
+				wait();
+			}catch(InterruptedException e){
+				
+			}	
+			
 		}
 		
-			notifyAll();
+			
 			arr[front] = i;
-	
-	
+			System.out.println("put "+i);
+			notify();	
 	}
 
 	synchronized void get(){
 		
-		if(rear>-1){
-			wait();
-		}
-		
+		if(rear<0){
+			try{
+				wait();
+			}catch(InterruptedException e){
 				
+			}	
+								
+		}
+		int i = arr[rear];
+		System.out.println("got "+ i);
+		notify();		
 	}		
 	
 
 }
 
-class producer {
-
-	queue q
-
-
-}
-
-class customer
+//-------------------------------------------producer
+class producer implements Runnable
 {
+	Thread t;
 	queue q;
+	producer(queue q){
+		this.q=q;
+		t = new Thread(this,"producer");
+		System.out.println("producer thread created");
+		t.start();	
+	}
+		
+	public void run(){
+		for(int i=0;i<10;i++){
+			q.put(i);
+		}
+		
+		
+	}
+
 }
+
+//-------------------------------------------customer
+class customer implements Runnable
+{	Thread r;
+	queue q;
+	customer(queue q){
+		this.q=q;
+		r = new Thread(this,"customer");	
+		System.out.println("customer class created");
+		r.start();
+	}
+
+	public void run(){
+		for(int i=0;i<10;i++){
+			q.get();
+		}
+	
+	}
+
+}
+
+//-------------------------------------------main
 public class exe5{
-    public static void main(){}
+	public static void main(String arg[]){
+		queue q = new queue();
+		producer p = new producer(q);
+		customer c = new customer(q);
+	}
 }
